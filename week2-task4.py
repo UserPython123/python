e60270b6dce8eb14e56608202a7fadb8835fd8ee
UserPython123 <37958068+UserPython123@users.noreply.github.com>
2018-04-01 00:09:01 +0530

    Week 2
    Basic python programs

---
+++ week2-task4.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 
+
+@author:
+"""
+
+print("Hello World")     # print command display output on screen          
+print("I am in my ISD class right now")
+
---
+++ week2-task5.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 
+
+@author:
+"""
+
+name = input("Hi there, what is your name? ")  #input takes input from the keyboard
+print("Hello")
+print(name)
+print("How are you?")
+
+
+
